0893632bfd7eb4ecbe8bbea989e704b60bc50adb
samuelhakala <samuel.hakala@codecontrol.fi>
2022-02-02 16:48:01 +0200

    -

--- src/form.jsx
+++ src/form.jsx
@@ -328,8 +328,7 @@ export default class ReactForm extends React.Component {
         case 'Range':
           return this.getInputElement(item);
         case 'CustomElement':
-          console.log(item.key, this.disableAutomaticNumberingForKeys)
-          if(!item.key.toLowerCase().includes("paragraph") || !item.key.toLowerCase().includes("title") || !this.disableAutomaticNumberingForKeys.includes(item.key)) {
+          if(!item.key.toString().toLowerCase().includes("paragraph") || !item.key.toString().toLowerCase().includes("title") || !this.disableAutomaticNumberingForKeys.includes(item.key.toString())) {
             const currNumber = questionNumber
             questionNumber = questionNumber + 1;
             return <div>
